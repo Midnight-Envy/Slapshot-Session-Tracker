1037a365e1a829319d1f3cd20b777f5875020881
Thomas Correia <thomas.correiap@gmail.com>
2026-06-30 13:00:41 -0400

    Feature 2 Complete

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import PlayerCard from "./PlayerCard";
+
 function PlayerList() {
-  return <h2>Tracked Players</h2>;
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/players")
+      .then((response) => response.json())
+      .then((playerData) => setPlayers(playerData));
+  }, []);
+
+  return (
+    <section>
+      <h2>Tracked Players</h2>
+
+      {players.length === 0 ? (
+        <p>No tracked players saved yet.</p>
+      ) : (
+        <div>
+          {players.map((player) => (
+            <PlayerCard key={player.id} player={player} />
+          ))}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default PlayerList;
